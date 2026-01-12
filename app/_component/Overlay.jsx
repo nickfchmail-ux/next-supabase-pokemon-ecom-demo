321d58fb4378cd61f1024f4cf66851805a031520
nick <nickfchmail@gmail.com>
2026-01-13 02:04:38 +0800

    tweaking mobile sidebar height

--- app/_component/Overlay.jsx
+++ app/_component/Overlay.jsx
@@ -1,7 +1,7 @@
 function Overlay({ children, view }) {
   return (
     <div
-      className={`${view === 'account' ? ' top-0 left-0 right-0 bottom-30 bg-primary-800' : 'backdrop-blur-[5px] inset-0'} fixed  z-10 grid place-items-center`}
+      className={`${view === 'account' ? ' inset-0 bg-primary-800' : 'backdrop-blur-[5px] inset-0'} fixed  z-10 grid place-items-center`}
     >
       {children}
     </div>
