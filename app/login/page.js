ca11953f4d49ded55eaee30ea8e8529fe5935190
nick <nickfchmail@gmail.com>
2026-02-09 13:41:18 +0800

    set up metadata for each page

--- app/login/page.js
+++ app/login/page.js
@@ -1,4 +1,14 @@
 import SignInView from '../_component/SignInView';
+
+export const metadata = {
+  title: 'Login',
+  description:
+    'please register your account with google email and provide your authentication from google for using your account information to set up your account on our ecommerce store.',
+};
+
+
+
+
 export default function Page() {
   return (
     <div className="h-[86vh] flex flex-col items-center justify-center space-y-6">
